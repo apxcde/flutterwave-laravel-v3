ac8855de84099894387c14a8ffa97bfc94fc7e4c
Kioko <kmkioko@gmail.com>
2020-11-02 12:34:58 +0300

    Main EventHandler and Rave Service with abstract Rave Service

---
+++ library/RaveServiceAbstract.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Laravel\Flutterwave;
+
+use Laravel\Flutterwave\EventHandler;
+
+abstract class RaveServiceAbstract
+{
+    protected $handler;
+
+    /**
+     * Sets the event hooks for all available triggers
+     * @param object $handler This is a class that implements the Event Handler Interface
+     * @return object
+     * */
+    public function eventHandler($handler)
+    {
+        $this->handler = $handler;
+        return $this;
+    }
+
+    /**
+     * Gets the event hooks for all available triggers
+     * @return object
+     * */
+    public function getEventHandler()
+    {
+        if ($this->handler) {
+            return $this->handler;
+        }
+
+        return new EventHandler;
+    }
+}
